f164d158e5c0ee054ad67524cd5ac5e94e1e408e
Luis Ramirez <luis.ramirez21x@outlook.com>
2022-01-27 21:13:38 -0500

    adding in basic components on app.js

---
+++ luis-ramirez-portfolio/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Project from './components/Project';
+
+function App() {
+    return (
+        <div>
+            <Header/>
+            <Project/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default App;
